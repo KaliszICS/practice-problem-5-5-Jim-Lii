699b947fea9b0a43b7b7b3586311df2e8dcb11cb
Jim-Lii <343347795@tdsb.ca>
2026-05-04 15:35:47 +0000

    erdtgfhjkhgfdsw

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,25 @@
+/*
+File Name: Arrays and iterating
+Author: Jim Li
+Date Created: May 4, 2026
+Date Last Modified: May 4, 2026
+*/
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static boolean isPalindrome(String word){
+		word = word.toLowerCase();
+		word = word.replace(" ", "");
+		char[] array = new char[word.length()];
+		for (int i = 0; i < word.length(); i++){
+			array[i] = word.charAt(i);
+		}
+		for (int i = 0; i < word.length(); i++){
+			if (array[i] != array[array.length - i - 1]){
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
